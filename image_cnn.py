cd774f82e9d80ed12f2fbd5f4e05f0d8f5455acc
garyyang01 <34117621+garyyang01@users.noreply.github.com>
2017-12-24 20:46:38 +0800

    predict image

--- image_cnn.py
+++ image_cnn.py
@@ -5,11 +5,10 @@ from keras.datasets import mnist
 import numpy as np
 
 model = load_model('mnist_cnn.h5')	#讀取model
-
-im = Image.open('image.bmp') 		#圖片路徑
+im_address="image.bmp"				#圖片路徑
+im = Image.open(im_address) 		
 #資料前處理
 im2arr = np.array(im)
-print (im2arr)
 im2arr=im2arr/255
 im2arr=im2arr.reshape(1,28,28,1) 
 ans=model.predict(im2arr)[0]
@@ -22,5 +21,5 @@ for n in [0,1,2,3,4,5,6,7,8,9]:
 		bestclass = str(n)
 		bestconf = ans[n]
 print (ans)#輸出結果信賴矩陣
-print ("class : "+str(bestclass)+" with "+str(bestconf)+" confidence.")#輸出結果
+print ("picture: "+im_address+" assigned to class "+str(bestclass)+" with "+str(bestconf)+" confidence.")#輸出結果
 
