480d99333200c27e15bf984d63da6cf677941545
garyyang01 <34117621+garyyang01@users.noreply.github.com>
2017-12-24 18:32:47 +0800

    predict image

--- image_cnn.py
+++ image_cnn.py
@@ -4,25 +4,23 @@ from PIL import Image
 from keras.datasets import mnist
 import numpy as np
 
-model = load_model('mnist_cnn.h5')
+model = load_model('mnist_cnn.h5')	#讀取model
 
-im = Image.open('./9.bmp')
+im = Image.open('image.bmp') 		#圖片路徑
+#資料前處理
 im2arr = np.array(im)
 print (im2arr)
 im2arr=im2arr/255
 im2arr=im2arr.reshape(1,28,28,1) 
 ans=model.predict(im2arr)[0]
 
-
-
+#one hot decoder
 bestclass = ''
 bestconf = -1
-
 for n in [0,1,2,3,4,5,6,7,8,9]:
 	if (ans[n] > bestconf):
 		bestclass = str(n)
 		bestconf = ans[n]
-
-print (ans)
-print ("class : "+str(bestclass)+" with "+str(bestconf)+" confidence.")
+print (ans)#輸出結果信賴矩陣
+print ("class : "+str(bestclass)+" with "+str(bestconf)+" confidence.")#輸出結果
 
